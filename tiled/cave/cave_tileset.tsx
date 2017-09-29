619acd70bf33939cb8fa329a04e5d2c3df9fbf76
Igor Borges <igor@borges.me>
2017-09-29 02:11:18 -0300

    external dungeon and cave tileset
    to be used interchangeably between maps of different types

---
+++ tiled/cave/cave_tileset.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="cave_tileset" tilewidth="64" tileheight="128" tilecount="240" columns="16">
+ <grid orientation="isometric" width="64" height="32"/>
+ <image source="tiled_cave.png" width="1024" height="1920"/>
+</tileset>
