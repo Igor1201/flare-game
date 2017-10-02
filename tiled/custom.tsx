871b399e40c0b2f80a887693f4c8e3e3d54ff9de
Igor Borges <igor@borges.me>
2017-10-02 20:31:22 -0300

    lower dungeon

---
+++ tiled/custom.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="custom" tilewidth="256" tileheight="256" tilecount="4" columns="0">
+ <tileoffset x="0" y="16"/>
+ <grid orientation="isometric" width="64" height="32"/>
+ <tile id="0">
+  <image width="256" height="256" source="../gimp/winter_tile_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="256" source="../gimp/winter_tile_2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="256" source="../gimp/winter_tile_back_1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="256" source="../gimp/winter_tile_back_2.png"/>
+ </tile>
+</tileset>
